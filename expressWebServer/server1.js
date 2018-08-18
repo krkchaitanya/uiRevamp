9f429ad600bfebfb5e15275bab279ae014941df6
krk chaitanya <krkchaitanya42@gmail.com>
2018-08-18 13:37:13 -0600

    sending response of JSON file

--- expressWebServer/server1.js
+++ expressWebServer/server1.js
@@ -5,10 +5,28 @@
 // cors --> cross origin request access
 const express = require("express");
 const path=require("path");
+const cors= require("cors");
+const bodyParser=require("body-parser");
+const fs=require("fs");
 const app=express();
 
+app.use(cors());
+app.use(bodyParser());
+
+// res.sendFile.......
 app.get('/',(req,res) => {
     res.sendFile(path.join(__dirname, "/../basic_html.html"));
+});
+
+// readFileSync(Sync ...blocking executuin of other stmt) vs readFile(Asynchronous ...runs in background...no blocking)
+app.get('/getUserInformation',(req,res)=>{
+    const userCardsFile=fs.readFileSync(path.join(__dirname,"mockResponses/getUserInformation.json"), "utf-8", (error,data) => {
+        if(error){
+            console.log(`Error fetching data...`);
+        }
+        return data;
+    });
+    res.send(userCardsFile);
 });
 
 
