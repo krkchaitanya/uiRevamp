e0340138e0450cad67c217a1dfbf50cd272861de
krk chaitanya <krkchaitanya42@gmail.com>
2018-11-04 21:58:25 -0600

    flex box init

---
+++ flex_box/flexServer.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+
+// app init
+const app = express();
+
+app.use(cors());
+app.use("/utils", express.static(path.join(__dirname,"./utils")));
+
+app.get("/", (req,res) => {
+    res.sendFile(path.join(__dirname,"./index.html"));
+});
+
+
+//port
+const port = process.env.PORT || 5657;
+
+// Run app on server
+app.listen(port, () => {
+    console.log('App is running on port -->', port);
+});
