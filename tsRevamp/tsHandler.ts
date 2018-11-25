bd8eef612aea280b98ae146bc6b66c5372f0973f
krk chaitanya <krkchaitanya42@gmail.com>
2018-11-25 15:09:47 -0700

    TS revamp and added transscript module to transpile the ts file to js

---
+++ tsRevamp/tsHandler.ts
@@ -0,0 +1,40 @@
+// Requirement .. npm i typescript --g
+// run command tsc example.ts / It gives an output file of name example.js
+// Typescript------>>>>>> is superset of JavaScript
+// Transcompile the Ts to regular browser understandable JavaScript
+// Used in larger applictions
+
+
+// Offers
+// Type check
+// class based objects
+// Modularity
+// Es6 Features
+// Syntax is closer to JAVA and other high level languages
+
+// String Number Boolean Array Any Void Null Tuple Enum Generics
+
+// USE ==>  Object oriented programming in JS / No Prototypes / Encapsulation - Functionality and resources all are at 1 place
+
+let myName: string;
+let myNumber: number;
+let myBool:boolean;
+let myvar:any;
+let cars:string[];
+
+myName="greatName";
+myNumber=982742983;
+myBool=false;
+myvar="wergjlw";
+cars=['Honda','toyota','Ford','subaru'];
+
+let stringArr:Array<string>;
+let numberArr:Array<number>;
+
+stringArr=['one','two','three'];
+numberArr=[2,3,43434];
+
+
+console.log(stringArr);
+console.log(numberArr);
+console.log(myName.slice(0,3));
