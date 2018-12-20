9b8dee9c53681122b4926acf6d55c8f09c1c4a53
krk chaitanya <krkchaitanya42@gmail.com>
2018-12-19 21:52:12 -0700

    Added prop-types dependency to check type of input params

--- reactRevamp/src/components/App.js
+++ reactRevamp/src/components/App.js
@@ -3,6 +3,25 @@ import PersonInfo from './FunctionalComponents/PersonInfo/PersonInfo';
 import AccordionSection from './FunctionalComponents/Accordion/AccordionSection';
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state={
+            accordionArrData: [
+                {
+                    header: 'AppHeader',
+                    bodyInfo: 'ljdhsfakhjafs  eadujfk awoeidszkhfaod f aouds uoh iuadshka su usodfghoisdfg oeris guois'
+                },
+                {
+                    header: 'AppHeader',
+                    bodyInfo: 'sdjghf pewiaujas ewap ufpsodiou uh espui oai sirf ujpae poersigporseg rpeisujgrp'
+                }, 
+                {
+                    header: 'AppHeader',
+                    bodyInfo: 'sediurg aweo oawuei idas sadhfsai s dfjhads fhfdsjk sahrfg fdkjhgasidfg uhjsdf siofd'
+                }
+            ]
+        }
+    }   
     render() {
         return (
             <div>
@@ -12,7 +31,7 @@ class App extends Component {
                 {/* person component */}
                 <PersonInfo />
                 <br />
-                <AccordionSection />
+                <AccordionSection accordionArrData= {this.state.accordionArrData}/>
             </div>
         );
     }
--- reactRevamp/src/components/FunctionalComponents/Accordion/AccordionSection.js
+++ reactRevamp/src/components/FunctionalComponents/Accordion/AccordionSection.js
@@ -1,36 +1,25 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class AccordionSection extends Component {
     constructor(props) {
         super();
         this.state = {
-            accordionInfo: [
-                {
-                    header: 'Heder1',
-                    bodyInfo: 'ljdhsfakhjafs  eadujfk awoeidszkhfaod f aouds uoh iuadshka su usodfghoisdfg oeris guois'
-                },
-                {
-                    header: 'Header2',
-                    bodyInfo: 'sdjghf pewiaujas ewap ufpsodiou uh espui oai sirf ujpae poersigporseg rpeisujgrp'
-                }, 
-                {
-                    header: 'Header3',
-                    bodyInfo: 'sediurg aweo oawuei idas sadhfsai s dfjhads fhfdsjk sahrfg fdkjhgasidfg uhjsdf siofd'
-                }
-            ]
+            accordionInfo: props.accordionArrData,
         }
     }
     
     render() {
 
+
         return(
             <div>
-                <h2>Accordion component...</h2>
+                <h2 className='componentHeading'>Accordion component...</h2>
                 {
                     this.state.accordionInfo.map((accordObj, index) => {
                         return (
                             <React.Fragment key={index}>
-                                <h2 className='accordHeader'>{accordObj.header}</h2>
+                                <h2 className='accordHeader'>{`${accordObj.header}..${index+1}`}</h2>
                                 <p className='accordBodyInfo'>{accordObj.bodyInfo}</p>
                             </React.Fragment>
                         );
@@ -38,7 +27,33 @@ class AccordionSection extends Component {
                 }
             </div>
         );
+
+
     }
+}
+
+AccordionSection.propTypes = {
+    accordionArrData: PropTypes.arrayOf(PropTypes.shape({
+        header: PropTypes.string,
+        bodyInfo: PropTypes.string
+    }))
+};
+
+AccordionSection.defaultProps = {
+    accordionArrData :[
+        {
+            header: 'Header',
+            bodyInfo: 'ljdhsfakhjafs  eadujfk awoeidszkhfaod f aouds uoh iuadshka su usodfghoisdfg oeris guois'
+        },
+        {
+            header: 'Header',
+            bodyInfo: 'sdjghf pewiaujas ewap ufpsodiou uh espui oai sirf ujpae poersigporseg rpeisujgrp'
+        }, 
+        {
+            header: 'Header',
+            bodyInfo: 'sediurg aweo oawuei idas sadhfsai s dfjhads fhfdsjk sahrfg fdkjhgasidfg uhjsdf siofd'
+        }
+    ]
 }
 
 export default AccordionSection;
