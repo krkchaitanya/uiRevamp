896aef14f1988a33f69ee024d5bbf6700090b489
krk chaitanya <krkchaitanya42@gmail.com>
2018-12-22 00:11:44 -0700

    Aded onclick functionality to tab to display content in body

--- reactRevamp/src/components/ReusableComponents/Navbar/navbar.js
+++ reactRevamp/src/components/ReusableComponents/Navbar/navbar.js
@@ -6,18 +6,34 @@ class Navbar extends Component {
     constructor(props) {
         super();
         this.state = {
-            navbarData: props.navbarArrayData 
+            navbarData: props.navbarArrayData,
+            tabBodyDataToBeDisplayed: props.navbarArrayData[0].tabBodyInfo
         }
     }
+
+    tabOnclick(tabNm) {
+        this.state.navbarData.filter((tabinfo) => {
+           if(tabinfo.tabName === tabNm) {
+                this.setState({
+                    tabBodyDataToBeDisplayed: tabinfo.tabBodyInfo
+                });
+           } 
+        });
+    }
+
     render() {
 
         return (
             <Fragment>
                 <nav className='navbar container'>
-                    <div><a href="#divone">Navone</a></div>
+                    {
+                        this.state.navbarData.map((ele, index) => {
+                            return <div key={index} onClick={() => this.tabOnclick(ele.tabName) }><a>{ele.tabName}</a></div>;
+                        })
+                    }
                 </nav>
                 <section className="navBody">
-
+                    <h3>{this.state.tabBodyDataToBeDisplayed}</h3>
                 </section>
             </Fragment>
         );
@@ -26,13 +42,26 @@ class Navbar extends Component {
 }
 
 Navbar.propTypes = {
-    navbarArrayData: PropTypes.any,
+    navbarArrayData: PropTypes.arrayOf(
+        PropTypes.shape({
+            tabName: PropTypes.string,
+            tabbodyinfo: PropTypes.string
+        })
+    ),
 }
 Navbar.defaultProps = {
     navbarArrayData: [
         {
             tabName: "Tabone",
             tabBodyInfo: "sdhf ausidhf iua jhsd vfiau asd sugd auyh fuds hiaeuwyfosd hersaoi geaori jvfdjkhgsgdfhigodsf sugd auyh fuds hiaeuwyfosd hersaoi geaori jvfdjkhgsgdfhigodsf"
+        },
+        {
+            tabName: "Tabtwo",
+            tabBodyInfo: "iuershgjv kfdh uisrhgf jdkfk s ujhgvdfkjghshfd giueurisews vfbvbsfd uighershg xnfckshdfuk hirueg hfdh gjkdfh urwie uhgujsdfguhg jiusd hui weyrug hiuersh euwyfosd hersaoi geaori jvfdjkhgsgdfhigodsf"
+        },
+        {
+            tabName: "TabThree",
+            tabBodyInfo: "godsf sugd auyh fuds hiaeuwyfosd hersaoi geaori jvfdjkhgsgdfhigodsf"
         }
     ]
 }
