f1534f226e18f0c1b735375005a8d5ab3e740895
krk chaitanya <krkchaitanya42@gmail.com>
2019-01-15 19:43:01 -0700

    Async Await Update

--- AsyncAwait/transcendPromises.js
+++ AsyncAwait/transcendPromises.js
@@ -39,3 +39,24 @@ createPost({
     title: "new great post",
     description: "dfljn dskhv kdhlsjvl kd kudyd gdukgy dukli"
 }).then(getPosts);
+
+// Promise.all   // Synchronus operations
+const theNewPromise1 = fetch('https://jsonplaceholder.typicode.com/users');
+const theNewPromise2 = new Promise((resolve,reject) => {
+    resolve("Its the new Promise2 functionslity");
+});
+
+Promise.all([theNewPromise1, theNewPromise2]).then((resObj) => {
+    console.log('theNewPromise2 -> ', resObj[1]);
+    resObj[0].json().then((data) => {
+        console.log(' Response data after maing fetch call -> ', data);
+    })
+});
+
+// ASYNC and AWait -> Built on Promises // Under the hood we deal with ES6 Promises
+async function fetchUserData() {
+    const userFetchOperation = await fetch('https://jsonplaceholder.typicode.com/users');
+    const userPromiseOutput = await userFetchOperation.json();
+    console.log('userPromiseOutput -> ', userPromiseOutput);
+}
+fetchUserData();
