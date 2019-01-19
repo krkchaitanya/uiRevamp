f81179efdd8c2bbdb9d0eccd51e1475056d2e885
krk chaitanya <krkchaitanya42@gmail.com>
2019-01-19 01:39:40 -0700

    updated query flow to test it using graphiql tool

--- graphQLNinja/Schema/schema.js
+++ graphQLNinja/Schema/schema.js
@@ -17,24 +17,26 @@ const {
 // JSON file paths
 const bookFilePath = path.join(__dirname, '../MockData/Books.json');
 
-const booksJsonDataPromise = () => {
-    return new Promise((resolve,reject) => {
-        fs.readFile(bookFilePath, {encoding: 'utf-8'}, (error, booksData) => {
-            if(!error){
-                resolve(booksData);
-            } else {
-                reject(error);
-            }
-        });
-    });
-};
- 
-const finalResult = booksJsonDataPromise().then((booksContent) => {
-    console.log(booksContent);
-    return booksContent
-});
+const booksJsonContent = JSON.parse(fs.readFileSync(bookFilePath, 'utf-8'));
+console.log('Mocked booksJsonData fetched -> ', booksJsonContent.books);
 
-console.log('--finalResult--', finalResult);
+// const booksJsonDataPromise = () => {
+//     return new Promise((resolve,reject) => {
+//         fs.readFile(bookFilePath, {encoding: 'utf-8'}, (error, booksData) => {
+//             if(!error){
+//                 resolve(JSON.parse(booksData));
+//             } else {
+//                 reject(error);
+//             }
+//         });
+//     });
+// };
+
+
+const books = 
+    [ { name: 'Name of the Wind', genre: 'Fantasy', id: "1" },
+      { name: 'The Final Empire', genre: 'Fantasy', id: "2" },
+      { name: 'The Long earth', genre: 'SCI-FI', id: "3" } ];
 
 const BookType = new GraphQLObjectType({
    name: 'Book',
@@ -51,10 +53,11 @@ const RootQuery = new GraphQLObjectType({
     fields: {
         book: {
             type: BookType,
-            args: {id: {type: GraphQLID}},
+            args: {id: {type: GraphQLString}},
             // resolve function is used to fetch data from DB or other sources
             resolve(parent,args) {
-                return _.find(finalResult, {books: {id: args.id}});
+                console.log('hitting resolve function...');
+                 return _.find(booksJsonContent.books, {id:args.id} );
             }
         }
     }
