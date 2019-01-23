b6653013cd1cc4f618f62ff6c7e0b3b87ffbd552
krk chaitanya <krkchaitanya42@gmail.com>
2019-01-22 22:08:03 -0700

    refactored graphQLServerTwo file

--- graphQLNinja/graphQLServerTwo.js
+++ graphQLNinja/graphQLServerTwo.js
@@ -11,80 +11,79 @@ const app = express();
 // Middlewares
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHTTP({
-    schema: buildSchema(`
+var schema = buildSchema(`
+    type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
 
-        type Event {
-            _id: ID!
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
- 
-        input EventInput {
-            title: String!
-            description: String!
-            price: Float!
-            date: String!
-        }
+    input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+    }
 
-        type RootQuery {
-            events: [Event!]!
-        }
-        
-        type RootMutation {
-            createEvent(eventInput: EventInput): Event
-        }
+    type RootQuery {
+        events: [Event!]!
+    }
 
-        schema {
-            query: RootQuery
-            mutation: RootMutation
-        }
+    type RootMutation {
+        createEvent(eventInput: EventInput): Event
+    }
 
-    `),
-    rootValue: {
+    schema {
+        query: RootQuery
+        mutation: RootMutation
+    }
+`);
 
-        events: () => {
-            return Event
-            .find()
-            .then(events => {
-                return events.map(event =>{
-                    return {...event._doc};
-                }); 
-            })
-            .catch(error => {
-                console.log(error);
-            });       
-        },
-
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: args.eventInput.price,
-                date: new Date(args.eventInput.date)
-            });
-            return event
-            .save()
-            .then(result => {
-                console.log(result);
-                return {...result._doc};
-            })
-            .catch(err => {
-                console.log(err);
-                throw err;
-            });
-        }
+var root = {
+    events: () => {
+        return Event
+        .find()
+        .then(events => {
+            return events.map(event =>{
+                return {...event._doc};
+            }); 
+        })
+        .catch(error => {
+            console.log(error);
+        });       
     },
+
+    createEvent: (args) => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: args.eventInput.price,
+            date: new Date(args.eventInput.date)
+        });
+        return event
+        .save()
+        .then(result => {
+            console.log(result);
+            return {...result._doc};
+        })
+        .catch(err => {
+            console.log(err);
+            throw err;
+        });
+    }
+  };
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
     graphiql: true
 }));
-
 
 app.get("/", (req,res) => {
     res.send("..Basic route implemented here..");
 });
-
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@projectz-f61na.mongodb.net/${process.env.MONGO_DB}?retryWrites=true`)
 .then(() => {
