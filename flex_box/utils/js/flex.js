fffc8d69c033d96f5b582c72e3f207a0b77219d3
krk chaitanya <krkchaitanya42@gmail.com>
2019-10-06 22:22:54 -0600

    refactored the signup section and added new supporting js

---
+++ flex_box/utils/js/flex.js
@@ -0,0 +1,15 @@
+function defaultfn() {
+    console.log("-- Entering in to the defaultfn function --");
+
+
+
+};
+defaultfn();
+
+
+
+document.getElementById("gamersignup-subt-btn").onclick = function() {
+    var username = document.getElementById("gamer-username").value;
+    var emailaddress = document.getElementById("gamer-emailaddress").value;
+    var password = document.getElementById("gamer-password").value;
+};
