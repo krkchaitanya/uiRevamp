04cca25f3532638e58ffa5e4de09a2f63bbe62c3
krk chaitanya <krkchaitanya42@gmail.com>
2019-10-24 21:16:43 -0600

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,17 @@
 const path = require("path");
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { NODE_ENV = 'development' } = process.env;
 
 module.exports = {
   entry:{
     main : './src/index.js',
-    es6_es7: './Spotlight/es6-es7/jsFeatures.js' 
+    es6_es7: './Spotlight/es6-es7/jsFeatures.js' ,
+    dropdown_fn_handler: './vanillaJS/dropdown_navigation/dropdown-fn-handler.js',
+    es6es7classes: './es2015-classes/es6es7classes.js'
     },
     output :{
-      path :path.resolve('./dist'),
-      filename: "[name].min.js"
+      path :path.join(__dirname, './dist'),
+      filename: "js/[name].min.js"
     },
     mode: NODE_ENV,
     module: {
@@ -28,14 +29,14 @@ module.exports = {
         ]
     },
     plugins: [
-        new HtmlWebpackPlugin({
-          title: 'survivor',
-          template: './Spotlight/es6-es7/jsFeatures.html'
-        })
+      new HtmlWebpackPlugin({
+        filename: "html/index.html"
+      })
     ],
     devServer: {
         port: 2811,
         compress: true,
+        contentBase: path.join(__dirname, "./dist/html"),
         proxy: {
             "/api": "http://localhost:2600"
         }
