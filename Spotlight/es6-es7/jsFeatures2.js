91318e83188bc5779d78f1089b9c924191a54965
krk chaitanya <krkchaitanya42@gmail.com>
2019-11-02 22:51:05 -0600

    window localStorage

--- Spotlight/es6-es7/jsFeatures2.js
+++ Spotlight/es6-es7/jsFeatures2.js
@@ -5,4 +5,27 @@ const survivorInfMaker = (name, community, desc) => {
     return {name, community, desc};
 };
 
+// window.localStorage
+// localStorage only deals with the strings.
+const travelerDtls = {
+    name: "andrew anklllo",
+    desctination: "salt lake city UTAH",
+    passion: "travelling",
+    age: "31"
+};
+
+localStorage.setItem("trvlDtls", JSON.stringify(travelerDtls));
+localStorage.getItem("trvlDtls");
+localStorage.clear();
+
+const randomNum =  33;
+localStorage.setItem("num", randomNum);
+if (!Number.isNaN(parseInt(localStorage.getItem("num")))) {
+    console.log("Successfully fetched the num value from localStorage");    
+} else {
+    console.log("failure in fetching the num value from localStorage");
+}
+localStorage.clear();
+
+
 export {ageCalculator, survivorInfMaker};
