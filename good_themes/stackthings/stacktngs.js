d63b424c8df41a3a3d10e948ac3dbe354c6be3db
krk chaitanya <krkchaitanya42@gmail.com>
2020-01-06 23:14:41 -0700

    stackthings theme dev continued

--- good_themes/stackthings/stacktngs.js
+++ good_themes/stackthings/stacktngs.js
@@ -1,1 +1,11 @@
 console.log("validating stacktngs js file");
+
+
+// navbar list items hover functionality
+// list item:: landing
+document.querySelector(".header_elements--navbar-landing").addEventListener("mouseenter", showContent);
+document.querySelector(".header_elements--navbar-landing").addEventListener("mouseleave", hideContent);
+function showContent() 
+{document.querySelector(".hovercontent > .hovercontent_landing").style.display = "block";}
+function hideContent()
+{document.querySelector(".hovercontent > .hovercontent_landing").style.display = "none";}
