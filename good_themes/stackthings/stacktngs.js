8491eddac7a5b23d3fa32ca76e60ce3dbd4418ec
krk chaitanya <krkchaitanya42@gmail.com>
2020-01-08 23:57:20 -0700

    hover function init

--- good_themes/stackthings/stacktngs.js
+++ good_themes/stackthings/stacktngs.js
@@ -1,11 +1,26 @@
 console.log("validating stacktngs js file");
 
 
-// navbar list items hover functionality
-// list item:: landing
-document.querySelector(".header_elements--navbar-landing").addEventListener("mouseenter", showContent);
-document.querySelector(".header_elements--navbar-landing").addEventListener("mouseleave", hideContent);
-function showContent() 
-{document.querySelector(".hovercontent > .hovercontent_landing").style.display = "block";}
-function hideContent()
-{document.querySelector(".hovercontent > .hovercontent_landing").style.display = "none";}
+// get navbar items list
+let navbarelements = [];
+let navitems = [];    
+navbarelements = document.querySelector(".header_elements--navbar").children;
+navbarelementsArr = Array.from(navbarelements);
+if (undefined != navbarelements && navbarelements.length > 0) {
+    let i = 0;
+    while(i < navbarelementsArr.length) {
+        navitems.push(navbarelementsArr[i].children.item(0).innerText);
+        i++;
+    }
+}
+
+
+document.querySelector(".header_elements--navbar-landing").addEventListener("mouseenter", showContent("landing"));
+document.querySelector(".header_elements--navbar-landing").addEventListener("mouseleave", hideContent("landing"));;
+
+
+// show and hide content of navbar items
+function showContent(element) 
+{document.querySelector(".hovercontent > .hovercontent_"+element).style.display = "block";}
+function hideContent(element)
+{document.querySelector(".hovercontent > .hovercontent_"+element).style.display = "none";}
