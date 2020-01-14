53ba618f8c3889fbcb7864932d7814c7b58004e6
krk chaitanya <krkchaitanya42@gmail.com>
2020-01-13 20:51:03 -0700

    css grid implementation init

--- good_themes/stackthings/stacktngs.js
+++ good_themes/stackthings/stacktngs.js
@@ -4,32 +4,28 @@
 })();
 
 
-// get navbar items list
-let navbarelements = [];
-let navitems = [];    
-navbarelements = document.querySelector(".header_elements--navbar").children;
-navbarelementsArr = Array.from(navbarelements);
-if (undefined != navbarelements && navbarelements.length > 0) {
-    let i = 0;
-    while(i < navbarelementsArr.length) {
-        navitems.push(navbarelementsArr[i].children.item(0).innerText);
-        i++;
-    }
-}
-
 // function definition - Toggle the navbar items content
 function toggleNavBarItemsContent(elem) {
     const navbarEleContent = document.getElementsByClassName("hovercontent_"+elem)[0];
-    document.querySelector(".header_elements--navbar-"+elem).addEventListener("mouseenter", function(elem) {
+    document.querySelector(".header_element--"+elem).addEventListener("mouseenter", function(elem) {
         navbarEleContent.style.display = "block";
     });
-    document.querySelector(".header_elements--navbar-"+elem).addEventListener("mouseleave", function(elem) {
+    document.querySelector(".header_element--"+elem).addEventListener("mouseleave", function(elem) {
         navbarEleContent.style.display = "none";
     });
 };
 
-// function declaration - function call
-toggleNavBarItemsContent("landing");
-toggleNavBarItemsContent("pages");
-toggleNavBarItemsContent("account");
-toggleNavBarItemsContent("documentation");
+
+// get navbar items list
+let navbarelements = [];
+let navitems = [];    
+navbarelements = document.querySelector(".header_elements").children;
+navbarelementsArr = Array.from(navbarelements);
+if (undefined != navbarelements && navbarelements.length > 0) {
+    let i = 1;
+    while(i < navbarelementsArr.length-1) {
+        toggleNavBarItemsContent(navbarelementsArr[i].children.item(0).innerText.toLowerCase());
+        navitems.push(navbarelementsArr[i].children.item(0).innerText);
+        i++;
+    }
+}
